27e6f9c3946024e0b4315cd6595d56cc64736810
eniac00 <abir.ahammed.bhuiyan@g.bracu.ac.bd>
2022-11-16 09:35:03 +0600

    important dot files

---
+++ .php_cli.php
@@ -0,0 +1,10 @@
+<?php
+
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+}
+
+include_once __DIR__.'/index.php';
+
+
