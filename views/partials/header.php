754b2c4bd6f5638bc8da915aa4be6bc43675f094
eniac00 <abir.ahammed.bhuiyan@g.bracu.ac.bd>
2022-11-18 23:49:19 +0600

    redo

--- views/partials/header.php
+++ views/partials/header.php
@@ -19,7 +19,7 @@ else {$title = "Error";}
   <!-- custom style sheet link -->
   <link rel="stylesheet" href="./assets/css/style.css">
   <!-- bootstrap cdn -->
-  <link rel="stylesheet" href="./assets/bootstrap-5.2.2/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
 <body>
