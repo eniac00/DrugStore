1ac7d02404d844059d6bbd87b71bc369be7993db
eniac00 <abir.ahammed.bhuiyan@g.bracu.ac.bd>
2022-11-26 14:28:24 +0600

    favicon added

--- views/partials/header.php
+++ views/partials/header.php
@@ -18,9 +18,9 @@ if (preg_match("/\/$/i", $_SERVER['REQUEST_URI']) || preg_match("/\?/i", $_SERVE
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
   <title><?php echo $title; ?></title>
 
+  <link rel="icon" type="image/png" href="./assets/favicon.png"/>
   <!-- custom style sheet link -->
   <link rel="stylesheet" href="./assets/css/style.css">
   <!-- bootstrap cdn -->
