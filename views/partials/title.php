a194bb29a74bd3818e440afc111edddd20223d4b
eniac00 <abir.ahammed.bhuiyan@g.bracu.ac.bd>
2022-12-03 19:59:07 +0600

    title logic change

--- views/partials/title.php
+++ views/partials/title.php
@@ -1,19 +1,34 @@
 <?php
 
-$uri = $_SERVER['REQUEST_URI'];
-
-if (preg_match("/admin-products$/i", $uri) || preg_match("/\?/i", $uri)) {
-  $title = "Admin-Products";
-} else if (preg_match("/\/$/i", $uri) || preg_match("/\?/i", $uri)) {
-  $title = "Home";
-} else if (preg_match("/login/i", $uri)) {
-  $title = "Login";
-} else if (preg_match("/registration/i", $uri)) {
-  $title = "Registration";
-} else if (preg_match("/admin-dashboard/i", $uri)) {
-  $title = "Admin-dashboard";
-} else if (preg_match("/admin-products/i", $uri)) {
-  $title = "Admin-Products";
-} else {
-  $title = "Error";
+switch(current(explode('?', $_SERVER['REQUEST_URI']))) {
+  case '/':
+    $title = 'Home';
+    break;
+  case '/login':
+    $title = 'Login';
+    break;
+  case '/registration':
+    $title = 'Registration';
+    break;
+  case '/admin-dashboard':
+    $title = 'Admin-Dashboard';
+    break;
+  case '/admin-products':
+    $title = 'Admin-Products';
+    break;
+  case '/admin-orders':
+    $title = 'Admin-Orders';
+    break;
+  case '/admin-customers':
+    $title = 'Admin-Customers';
+    break;
+  case '/customer-profile':
+    $title = 'Profile';
+    break;
+  case '/customer-orders':
+    $title = 'Orders';
+    break;
+  default:
+    $title = 'Error';
+    break;
 }
